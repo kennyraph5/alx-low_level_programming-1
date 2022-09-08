a91376b0b0992cb49120215fea1852ac8055beef
samuelonoh <samuelonoh12345@gmail.com>
2022-09-08 13:29:53 -0700

    task done

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/*
+ * main - Entry point
+ *
+ * Description: Print programming is like building a multilingual puzzle
+ *
+ * Return: Always O (Success)
+ */
+int main(void)
+{
+     puts("\"Programming is like building a multilingual puzzle");
+     Return (0);
+}
