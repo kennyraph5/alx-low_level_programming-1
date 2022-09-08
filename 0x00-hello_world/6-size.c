9bd832617e221fe1b573dcfbf5edef6326f698e3
samuelonoh <samuelonoh12345@gmail.com>
2022-09-08 14:46:27 -0700

    task done

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-/*
+/**
  * main - pint the size of various types
  *
  * Description: prints the size of various types on the computer, it is compiled
@@ -12,10 +12,10 @@
 
 int main(void)
 {
-   printf("size of a char: %d byte(s)\n", (int) sizeof(char));
-   printf("size of an int: %d byte(s)\n", (int) sizeof(int));
-   printf("size of a long int: %d byte(s)\n", (int) sizeof(long int));
-   printf("size of a long long int: %d byte(s)\n", (int) sizeof(long long int));
-   printf("size of a float: %d byte(s)\n", (int) sizeof(float));
-   return (0);
+printf("size of a char: %d byte(s)\n", (int) sizeof(char));
+printf("size of an int: %d byte(s)\n", (int) sizeof(int));
+printf("size of a long int: %d byte(s)\n", (int) sizeof(long int));
+printf("size of a long long int: %d byte(s)\n", (int) sizeof(long long int));
+printf("size of a float: %d byte(s)\n", (int) sizeof(float));
+return (0);
 }
