3e4e3f0e7f0192187e39b567b6373cbaee3eefa6
ONOH SAMUEL <samuelonoh12345@gmail.com>
2022-09-19 19:26:52 +0100

    Update 4-print_rev.c
    done

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,45 +1,25 @@
 #include "main.h"
-
-int get_length(char *);
+#include <stdio.h>
 
 /**
- * print_rev - prints a string to the screen in reverse followed by a newline
- * @s: String to be printed in reverse
- *
+ * print_rev - prints string backwards
+ * @s: string
  * Return: void
  */
+
 void print_rev(char *s)
 {
-	int length;
-	int i;
+	int i = 0;
 
-	length = get_length(s);
-	for (i = length - 1; i >= 0; i--)
-	{
-		_putchar(s[i]);
-	}
-	_putchar('\n');
-}
-
-
-/**
- * get_length - returns the length of a String
- * @s: String whose length we want to determine
- *
- * Return: Length of String
- */
-int get_length(char *s)
-{
-	int length;
-	int i;
-
-	length = 0;
-	i = 0;
 	while (s[i] != '\0')
 	{
-		++length;
 		i++;
 	}
-
-	return (length);
+	i--;
+	while (i >= 0)
+	{
+		_putchar(s[i]);
+		i--;
+	}
+	_putchar('\n');
 }
